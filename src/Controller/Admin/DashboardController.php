9b80a73e4f9208e2a2beb315e9e1125dc9afd398
“Filip <filip@bratia.sk>
2021-02-04 09:30:12 +0100

    Fix admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\NcziMorningEmail;
+use App\Entity\Raw\NcziMorningEmail;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
