315804bbdb148cca802aa8843406dd840b4e5736
“Filip <filip@bratia.sk>
2021-02-06 21:58:14 +0100

    Fix migrations

--- migrations/Version20210204163814.php
+++ migrations/Version20210204163814.php
@@ -20,16 +20,12 @@ final class Version20210204163814 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX idx_4285551c43625d9f ON raw_nczi_vaccinations');
         $this->addSql('CREATE INDEX IDX_31B65F6243625D9F ON raw_nczi_vaccinations (updated_at)');
-        $this->addSql('ALTER TABLE vaccine CHANGE code code VARCHAR(255) CHARACTER SET ASCII NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP INDEX idx_31b65f6243625d9f ON raw_nczi_vaccinations');
-        $this->addSql('CREATE INDEX IDX_4285551C43625D9F ON raw_nczi_vaccinations (updated_at)');
-        $this->addSql('ALTER TABLE vaccine CHANGE code code VARCHAR(255) CHARACTER SET ascii NOT NULL COLLATE `ascii_general_ci`');
     }
 }
